0b20755443a61776b46553d6929930355e8d82c3
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-02-18 05:35:58 +0800

    refactor(change-case): use change-case package

--- src/utils/change-case.ts
+++ src/utils/change-case.ts
@@ -1,9 +1,9 @@
-import camelCase from "camelcase"
+import { camelCase, pascalCase } from "change-case"
 
 export function toCamelCase(str: string): string {
   return camelCase(str)
 }
 
 export function toPascalCase(str: string): string {
-  return camelCase(str, { pascalCase: true })
+  return pascalCase(str)
 }
