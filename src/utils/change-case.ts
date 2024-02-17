f961653db7b2d9c7f8bd38c037d7e305f6d42f18
Kiki Irwan Maulana <48400192+kim-dev-git@users.noreply.github.com>
2024-02-18 05:28:31 +0800

    refactor(change-case): use camelCase package

--- src/utils/change-case.ts
+++ src/utils/change-case.ts
@@ -1,15 +1,9 @@
+import camelCase from "camelcase"
+
 export function toCamelCase(str: string): string {
-  return str
-    .split(/[^A-Za-z]+/)
-    .map((word, index) => {
-      return index === 0
-        ? word.toLowerCase()
-        : word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
-    })
-    .join("")
+  return camelCase(str)
 }
 
 export function toPascalCase(str: string): string {
-  // Make the first letter uppercase and keep rest lowercase
-  return str.charAt(0).toUpperCase() + str.slice(1)
+  return camelCase(str, { pascalCase: true })
 }
